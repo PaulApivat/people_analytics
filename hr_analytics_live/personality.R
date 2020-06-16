4fe7cb7704f128af41a710fdfba31ba42d60907e
paulapivat <paul.apivat@gmail.com>
2020-06-16 12:57:51 +0700

    add personality.R file

---
+++ hr_analytics_live/personality.R
@@ -0,0 +1,6 @@
+# R version 3.6.3 (2020-02-29) -- "Holding the Windsock"
+# RStudio Version 1.2.5042
+
+# Case Study: Personality Insights (IBM Watson Personality Insights)
+# source: https://hranalyticslive.netlify.app/16-personality.html
+
